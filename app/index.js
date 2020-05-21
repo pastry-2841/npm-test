ebbdb7bb9d7a4568c3ca1499348445b1fdfc0814
yutang <yutang@dian.so>
2020-05-21 20:58:51 +0800

    fix

--- app/index.js
+++ app/index.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useReducer, useContext } from 'react'
 
 function App (props) {
-  const [state, dispatch] = useReducer(model.reducer, model.state)
   return (
     <div>
       aaaaa
