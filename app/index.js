c91a4c4f1b40790696c68e3f57c9bc20a989491c
yutang <yutang@dian.so>
2020-05-21 20:50:05 +0800

    测试

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useReducer, useContext } from 'react'
+
+function App (props) {
+  const [state, dispatch] = useReducer(model.reducer, model.state)
+  return (
+    <div>
+      aaaaa
+    </div>
+  )
+}
+
+export default App
